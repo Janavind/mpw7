042b8b4dde2aebaa77e9812bc70cdb3cc78d9489
jaravind <jaravind@aggies.ncat.edu>
2022-10-03 15:04:23 -0700

    modified:   def/user_project_wrapper.def 	modified:   gds/user_project_wrapper.gds 	modified:   lef/user_project_wrapper.lef 	modified:   mag/user_project_wrapper.mag 	modified:   maglef/user_project_wrapper.mag 	modified:   openlane/user_proj_example/config.tcl 	modified:   openlane/user_project_wrapper/config.tcl 	modified:   openlane/user_project_wrapper/macro.cfg 	modified:   spi/lvs/user_project_wrapper.spice 	modified:   verilog/gl/user_project_wrapper.v 	modified:   verilog/includes/includes.rtl.caravel_user_project 	deleted:    verilog/rtl/user_proj_example.v 	modified:   verilog/rtl/user_project_wrapper.v
    	caravel/
    	def/macro_7.def
    	dependencies/

--- openlane/user_project_wrapper/config.tcl
+++ openlane/user_project_wrapper/config.tcl
@@ -16,14 +16,14 @@
 # Base Configurations. Don't Touch
 # section begin
 
-set ::env(PDK) $::env(PDK)
+set ::env(PDK) "sky130B"
 set ::env(STD_CELL_LIBRARY) "sky130_fd_sc_hd"
 
 # YOU ARE NOT ALLOWED TO CHANGE ANY VARIABLES DEFINED IN THE FIXED WRAPPER CFGS 
-source $::env(DESIGN_DIR)/fixed_dont_change/fixed_wrapper_cfgs.tcl
+source $::env(CARAVEL_ROOT)/openlane/user_project_wrapper/fixed_wrapper_cfgs.tcl
 
 # YOU CAN CHANGE ANY VARIABLES DEFINED IN THE DEFAULT WRAPPER CFGS BY OVERRIDING THEM IN THIS CONFIG.TCL
-source $::env(DESIGN_DIR)/fixed_dont_change/default_wrapper_cfgs.tcl
+source $::env(CARAVEL_ROOT)/openlane/user_project_wrapper/default_wrapper_cfgs.tcl
 
 set script_dir [file dirname [file normalize [info script]]]
 
@@ -39,38 +39,89 @@ set ::env(VERILOG_FILES) "\
 
 ## Clock configurations
 set ::env(CLOCK_PORT) "user_clock2"
-set ::env(CLOCK_NET) "mprj.clk"
+set ::env(CLOCK_NET) "clk"
 
 set ::env(CLOCK_PERIOD) "10"
-
-## Internal Macros
-### Macro PDN Connections
-set ::env(FP_PDN_MACRO_HOOKS) "\
-	mprj vccd1 vssd1 vccd1 vssd1"
-
-### Macro Placement
-set ::env(MACRO_PLACEMENT_CFG) $script_dir/macro.cfg
+set ::env(PL_BASIC_PLACEMENT) 0
 
 ### Black-box verilog and views
+#set ::env(VERILOG_FILES_BLACKBOX) "\
+#	$::env(CARAVEL_ROOT)/verilog/rtl/defines.v \
+#	$script_dir/../../verilog/rtl/user_proj_example.v"
+
+## Internal Macros
+#### Macro PDN Connections
+set ::env(FP_PDN_MACRO_HOOKS) "\
+      u_macro_7 vccd1 vssd1 vccd1 vssd1 "
+ #       u_macro_10 vccd1 vssd1 \  
+#	u_macro_13 vccd1 vssd1"
+
+        ### Macro Placement
+set ::env(MACRO_PLACEMENT_CFG) $script_dir/macro.cfg
+### Black-box verilog and views
 set ::env(VERILOG_FILES_BLACKBOX) "\
-	$::env(CARAVEL_ROOT)/verilog/rtl/defines.v \
-	$script_dir/../../verilog/rtl/user_proj_example.v"
+        $::env(CARAVEL_ROOT)/verilog/rtl/defines.v \
+        $script_dir/../../verilog/rtl/macro_7.v "
+#       $script_dir/../../verilog/rtl/macro_10.v \
+#	$script_dir/../../verilog/rtl/macro_13.v \
+#	$script_dir/../../verilog/rtl/macro_15.v \
+#	$script_dir/../../verilog/rtl/macro_decap_3.v \
+#	$script_dir/../../verilog/rtl/macro_decap_12.v \
+#	$script_dir/../../verilog/rtl/macro_no_decap.v \
+#	$script_dir/../../verilog/rtl/macro_no_fill.v "
 
-set ::env(EXTRA_LEFS) "\
-	$script_dir/../../lef/user_proj_example.lef"
+set ::env(EXTRA_LEFS) "\ 
+	$script_dir/../../lef/macro_7.lef "
+#        $script_dir/../../lef/macro_10.lef \
+#	$script_dir/../../lef/macro_13.lef \
+#	$script_dir/../../lef/macro_15.lef \
+#	$script_dir/../../lef/macro_decap_3.lef \
+#	$script_dir/../../lef/macro_decap_12.lef \
+#	$script_dir/../../lef/macro_no_decap.lef \
+#	$script_dir/../../lef/macro_no_fill.lef "
 
-set ::env(EXTRA_GDS_FILES) "\
-	$script_dir/../../gds/user_proj_example.gds"
+set ::env(EXTRA_GDS_FILES) "\ 
+	$script_dir/../../gds/macro_7.gds"
+#       $script_dir/../../gds/macro_10.gds \
+#$script_dir/../../gds/macro_13.gds \
+#$script_dir/../../gds/macro_15.gds \
+#$script_dir/../../gds/macro_decap_3.gds \
+#$script_dir/../../gds/macro_decap_12.gds \
+#$script_dir/../../gds/macro_no_decap.gds \
+#$script_dir/../../gds/macro_no_fill.gds "
+
+set ::env(FP_PDN_ENABLE_MACROS_GRID) "1"
+set ::env(FP_PDN_ENABLE_GLOBAL_CONNECTIONS) "1"
+
+#set ::env(VDD_NETS) "vccd1 vccd2 vdda1 vdda2"
+#set ::env(GND_NETS) "vssd1 vssd2 vssa1 vssa2"
+
+#set ::env(VDD_PIN) "vccd1"
+#set ::env(GND_PIN) "vssd1"
+
+ ## Internal Macros
+ ##### Macro PDN Connections
+#    set ::env(FP_PDN_MACRO_HOOKS) "\
+#        user_proj_example vccd1 vssd1"
+ ### Macro Placement
+ #   set ::env(MACRO_PLACEMENT_CFG) $script_dir/macro.cfg
 
 # set ::env(GLB_RT_MAXLAYER) 5
-set ::env(RT_MAX_LAYER) {met4}
+set ::env(RT_MAX_LAYER) "met4"
 
 # disable pdn check nodes becuase it hangs with multiple power domains.
 # any issue with pdn connections will be flagged with LVS so it is not a critical check.
-set ::env(FP_PDN_CHECK_NODES) 0
+#set ::env(SYNTH_STRATEGY) "AREA 0"
+set ::env(FP_PDN_CHECK_NODES) 1
+
+#set ::env(GLB_RT_ALLOW_CONGESTION) 1
+
+set ::env(GLB_RT_ADJUSTMENT) "0.25"
+#set ::env(FP_PDN_POWER_STRAPS) "vccd1 vssd1 1, vccd2 vssd2 0, vdda1 vssa1 0, vdda2 vssa2 0"
+set ::env(MAGIC_WRITE_FULL_LEF) 1
 
 # The following is because there are no std cells in the example wrapper project.
-set ::env(SYNTH_TOP_LEVEL) 1
+set ::env(SYNTH_TOP_LEVEL) 0
 set ::env(PL_RANDOM_GLB_PLACEMENT) 1
 
 set ::env(PL_RESIZER_DESIGN_OPTIMIZATIONS) 0
@@ -84,5 +135,4 @@ set ::env(DIODE_INSERTION_STRATEGY) 0
 set ::env(FILL_INSERTION) 0
 set ::env(TAP_DECAP_INSERTION) 0
 set ::env(CLOCK_TREE_SYNTH) 0
-
 
--- verilog/gl/user_project_wrapper.v
+++ verilog/gl/user_project_wrapper.v
@@ -1,5 +1,4 @@
-module user_project_wrapper (user_clock2,
-    vccd1,
+module user_project_wrapper (vccd1,
     vccd2,
     vdda1,
     vdda2,
@@ -13,19 +12,16 @@ module user_project_wrapper (user_clock2,
     wbs_cyc_i,
     wbs_stb_i,
     wbs_we_i,
-    analog_io,
     io_in,
     io_oeb,
     io_out,
     la_data_in,
     la_data_out,
     la_oenb,
-    user_irq,
     wbs_adr_i,
     wbs_dat_i,
     wbs_dat_o,
     wbs_sel_i);
- input user_clock2;
  input vccd1;
  input vccd2;
  input vdda1;
@@ -40,28 +36,57 @@ module user_project_wrapper (user_clock2,
  input wbs_cyc_i;
  input wbs_stb_i;
  input wbs_we_i;
- inout [28:0] analog_io;
  input [37:0] io_in;
  output [37:0] io_oeb;
  output [37:0] io_out;
  input [127:0] la_data_in;
  output [127:0] la_data_out;
  input [127:0] la_oenb;
- output [2:0] user_irq;
  input [31:0] wbs_adr_i;
  input [31:0] wbs_dat_i;
  output [31:0] wbs_dat_o;
  input [3:0] wbs_sel_i;
 
 
- user_proj_example mprj (.vccd1(vccd1),
+ macro_7 u_macro_7 (.vccd1(vccd1),
     .vssd1(vssd1),
-    .wb_clk_i(wb_clk_i),
     .wb_rst_i(wb_rst_i),
     .wbs_ack_o(wbs_ack_o),
     .wbs_cyc_i(wbs_cyc_i),
     .wbs_stb_i(wbs_stb_i),
     .wbs_we_i(wbs_we_i),
+    .io_active({la_data_in[31],
+    la_data_in[30],
+    la_data_in[29],
+    la_data_in[28],
+    la_data_in[27],
+    la_data_in[26],
+    la_data_in[25],
+    la_data_in[24],
+    la_data_in[23],
+    la_data_in[22],
+    la_data_in[21],
+    la_data_in[20],
+    la_data_in[19],
+    la_data_in[18],
+    la_data_in[17],
+    la_data_in[16],
+    la_data_in[15],
+    la_data_in[14],
+    la_data_in[13],
+    la_data_in[12],
+    la_data_in[11],
+    la_data_in[10],
+    la_data_in[9],
+    la_data_in[8],
+    la_data_in[7],
+    la_data_in[6],
+    la_data_in[5],
+    la_data_in[4],
+    la_data_in[3],
+    la_data_in[2],
+    la_data_in[1],
+    la_data_in[0]}),
     .io_in({io_in[37],
     io_in[36],
     io_in[35],
@@ -176,9 +201,6 @@ module user_project_wrapper (user_clock2,
     io_out[2],
     io_out[1],
     io_out[0]}),
-    .irq({user_irq[2],
-    user_irq[1],
-    user_irq[0]}),
     .la_data_in({la_data_in[127],
     la_data_in[126],
     la_data_in[125],
